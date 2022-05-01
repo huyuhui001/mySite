e328692cc9213a9d42a4e498df60d084b6273862
huyuhui001 <h_yuhui@hotmail.com>
2022-05-01 16:58:39 +0800

    Demo

---
+++ Demo/Spider/setting.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8 -*-
+
+#
+# Configuration
+#
+
+import os
+
+# Define the path to save data
+BASE_PATH = os.path.dirname(
+    os.path.realpath(__file__)
+)
+
+IMG_PATH = os.path.join(
+    BASE_PATH, 'images'
+)
