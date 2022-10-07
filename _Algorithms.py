e9c643a8fffce2784ed0fa826c06bca7ce86faeb
huyuhui001 <h_yuhui@hotmail.com>
2022-10-07 17:40:12 +0800

    tmp file

--- _Algorithms.py
+++ _Algorithms.py
@@ -3,61 +3,34 @@
 # -*- coding: utf-8 -*-
 
 
-class Stack():
+class Queue():
     def __init__(self):
         self.items = []
-
+    
     def isEmpty(self):
         return self.items == []
-
-    def push(self, item):
-        self.items.append(item)
     
-    def pop(self):
+    def enqueue(self, item):
+        return self.items.insert(0, item)
+    
+    def dequeue(self):
         return self.items.pop()
-    
-    def peek(self):
-        return self.items[len(self.items) - 1]
     
     def size(self):
         return len(self.items)
 
 
-def matches(open, close):
-    opens = "([{"
-    closers = ")]}"
-
-    return opens.index(open) == closers.index(close)
-
-
-def parChecker(symbolString):
-    s = Stack()
-
-    matched = True
-    index = 0
-
-    while index < len(symbolString) and matched:
-        symbol = symbolString[index]
-
-        if symbol in "([{":
-            s.push(symbol)
-        else:
-            if s.isEmpty():
-                matched = False
-            else:
-                top = s.pop()
-                if not matches(top, symbol):
-                    matched = False
-        
-        index = index + 1
-
-    if matched and s.isEmpty():
-        return True, f"{symbolString}"
-    else:
-        return False
-
-
 if __name__ == '__main__':
-    parChecker("([[{}])")
-    parChecker("([{}])")
+    q = Queue()
+    q.isEmpty()
+    q.enqueue(2)
+    q.enqueue('h')
+    q.size()
+    q.isEmpty()
+    q.dequeue()
+    q.size()
+
+
+
+print("hello\r123")
 
