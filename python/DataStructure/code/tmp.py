3ea1b9cbf6f1bd1d78c86efe02fa83e5533a5818
huyuhui001 <h_yuhui@hotmail.com>
2023-09-03 18:54:24 +0800

    Deployed cb6f962 with MkDocs version: 1.3.1

--- python/DataStructure/code/tmp.py
+++ python/DataStructure/code/tmp.py
@@ -1,133 +1,87 @@
-def binarySearch(target, sortedLyst):
-    left = 0
-    right = len(sortedLyst) - 1
-
-    print("%5s%10s%10s" % ("left", "midpoint", "right"))
-
-    while left <= right:
-        midpoint = (left + right) // 2
-
-        print("%5s%10s%10s" % (left, midpoint, right))
-
-        if target == sortedLyst[midpoint]:
-            return midpoint
-        elif target < sortedLyst[midpoint]:
-            right = midpoint - 1
-        else:
-            left = midpoint + 1
-
-    return -1
+import random
 
 
-def letter_position(myLetter):
-    letter = myLetter.lower()  # 转成小写字母
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-    if letter in alphabet:
-        return alphabet.index(letter) + 1  # 位置按1～26计算
-    else:
-        return None  # 非字母
+def swap(lyst, i, j):
+    """交换元素位置为i和j的元素"""
+    temp = lyst[i]
+    lyst[i] = lyst[j]
+    lyst[j] = temp
 
 
-def dictSearch(target, sortedLyst):
-    left = 0
-    right = len(sortedLyst) - 1
-
-    # 首字母在字母表中的百分位
-    letter_position_range = letter_position(target[0]) * 100 // 26
-    # 按照所得的首字母在字母表中的百分位，作为给定字串中设定搜索起始百分位
-    midpoint = letter_position_range * (len(sortedLyst) - 1) // 100
-
-    print("%5s%10s%10s" % ("left", "midpoint", "right"))
-
-    while left <= right:
-        print("%5s%10s%10s" % (left, midpoint, right))
-
-        if target == sortedLyst[midpoint]:
-            return midpoint
-        elif target < sortedLyst[midpoint]:
-            right = midpoint - 1
-        else:
-            left = midpoint + 1
-
-        midpoint = (left + right) // 2
-
-    return -1
+def quicksort(lyst):
+    # left的初始值是0
+    # right的初始值是列表长度减1
+    quicksortHelper(lyst, 0, len(lyst) - 1)
 
 
-def main():
-    myList = [
-        "Bob", "Charlie", "Eva", "Alice", "Grace", "David", "Smith", "Frank",
-        "Zoe", "Jack"
-    ]
-    sortedList = sorted(myList)
-    print(sortedList)
+def quicksortHelper(lyst, left, right):
+    print(lyst)
+    if left < right:
+        pivotLocation = partition(lyst, left, right)
+        quicksortHelper(lyst, left, pivotLocation - 1)
+        quicksortHelper(lyst, pivotLocation + 1, right)
 
-    print("=====call binarySearch=====")
-    locatedIndex = binarySearch("Alice", sortedList)
-    print("Found", sortedList[locatedIndex], "in position", locatedIndex)
 
-    print("=====call dictSearch=====")
-    locatedIndex = dictSearch("Alice", sortedList)
-    print("Found", sortedList[locatedIndex], "in position", locatedIndex)
+def partition(lyst, left, right):
+    """对列表进行分区"""
+    # 找到基准元素（pivot），并和最后一个元素互换
+    middle = (left + right) // 2
+    pivot = lyst[middle]
+    lyst[middle] = lyst[right]
+    lyst[right] = pivot
+
+    # 设定边界元素（boundary point），初始是第一个元素
+    boundary = left
+    print("pivot: ", pivot, "boundary: ", lyst[boundary])
+
+    # 把所有小于基准的元素都移动到边界的左边
+    for index in range(left, right):
+        if lyst[index] < pivot:
+            swap(lyst, index, boundary)
+            boundary += 1
+    
+    # 交换基准元素和边界元素
+    swap(lyst, right, boundary)
+    print(lyst)
+    return boundary
+
+
+def main(size=20, sort=quicksort):
+    # lyst = []
+    # for count in range(size):
+    #     lyst.append(random.randint(1, size + 1))
+    lyst = [12, 19, 17, 18, 14, 11, 15, 13, 16]
+    # print(lyst)
+    sort(lyst)
+    # print(lyst)
 
 
 if __name__ == "__main__":
     main()
 
-# 运算结果：
-# 搜索Alice
-# ['Alice', 'Bob', 'Charlie', 'David', 'Eva', 'Frank', 'Grace', 'Jack', 'Smith', 'Zoe']
-# =====call binarySearch=====
-#  left  midpoint     right
-#     0         4         9
-#     0         1         3
-#     0         0         0
-# Found Alice in position 0
-# =====call dictSearch=====
-#  left  midpoint     right
-#     0         0         9
-# Found Alice in position 0
-
-# 搜索Bob
-# ['Alice', 'Bob', 'Charlie', 'David', 'Eva', 'Frank', 'Grace', 'Jack', 'Smith', 'Zoe']
-# =====call binarySearch=====
-#  left  midpoint     right
-#     0         4         9
-#     0         1         3
-# Found Bob in position 1
-# =====call dictSearch=====
-#  left  midpoint     right
-#     0         0         9
-#     1         5         9
-#     1         2         4
-#     1         1         1
-# Found Bob in position 1
-
-# 搜索Smith
-# ['Alice', 'Bob', 'Charlie', 'David', 'Eva', 'Frank', 'Grace', 'Jack', 'Smith', 'Zoe']
-# =====call binarySearch=====
-#  left  midpoint     right
-#     0         4         9
-#     5         7         9
-#     8         8         9
-# Found Smith in position 8
-# =====call dictSearch=====
-#  left  midpoint     right
-#     0         6         9
-#     7         8         9
-# Found Smith in position 8
-
-# 搜索Zoe
-# ['Alice', 'Bob', 'Charlie', 'David', 'Eva', 'Frank', 'Grace', 'Jack', 'Smith', 'Zoe']
-# =====call binarySearch=====
-#  left  midpoint     right
-#     0         4         9
-#     5         7         9
-#     8         8         9
-#     9         9         9
-# Found Zoe in position 9
-# =====call dictSearch=====
-#  left  midpoint     right
-#     0         9         9
-# Found Zoe in position 9
+# 运行结果：
+# [12, 19, 17, 18, 14, 11, 15, 13, 16]
+# pivot:  14 boundary:  12
+# [12, 11, 13, 14, 16, 19, 15, 17, 18]
+# [12, 11, 13, 14, 16, 19, 15, 17, 18]
+# pivot:  11 boundary:  12
+# [11, 13, 12, 14, 16, 19, 15, 17, 18]
+# [11, 13, 12, 14, 16, 19, 15, 17, 18]
+# [11, 13, 12, 14, 16, 19, 15, 17, 18]
+# pivot:  13 boundary:  12
+# [11, 12, 13, 14, 16, 19, 15, 17, 18]
+# [11, 12, 13, 14, 16, 19, 15, 17, 18]
+# [11, 12, 13, 14, 16, 19, 15, 17, 18]
+# [11, 12, 13, 14, 16, 19, 15, 17, 18]
+# pivot:  15 boundary:  16
+# [11, 12, 13, 14, 15, 19, 18, 17, 16]
+# [11, 12, 13, 14, 15, 19, 18, 17, 16]
+# [11, 12, 13, 14, 15, 19, 18, 17, 16]
+# pivot:  18 boundary:  19
+# [11, 12, 13, 14, 15, 16, 17, 18, 19]
+# [11, 12, 13, 14, 15, 16, 17, 18, 19]
+# pivot:  16 boundary:  17
+# [11, 12, 13, 14, 15, 16, 17, 18, 19]
+# [11, 12, 13, 14, 15, 16, 17, 18, 19]
+# [11, 12, 13, 14, 15, 16, 17, 18, 19]
+# [11, 12, 13, 14, 15, 16, 17, 18, 19]
