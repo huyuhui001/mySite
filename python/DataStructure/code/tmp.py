5af0dd6bac279d48bdc0884bd88d387fc85686e4
huyuhui001 <h_yuhui@hotmail.com>
2023-09-27 21:57:56 +0800

    Deployed d07a5cf with MkDocs version: 1.3.1

--- python/DataStructure/code/tmp.py
+++ python/DataStructure/code/tmp.py
@@ -33,25 +33,42 @@ class Array(object):
 
 
 def main(size=10):
+    # 初始值
     DEFAULT_CAPACITY = 5
     logicalSize = 0
     my_array = Array(DEFAULT_CAPACITY)
 
+    # 打印输出初始数组信息
     print("Initial array is: ", my_array)
     print("Len of the array: ", my_array.__len__())
 
+    # 给数组赋值
     for i in range(len(my_array)):
         my_array[i] = i
 
-    print("The array is: ", my_array.items)
+    print("The array is: ", my_array.items)  # 打印输出数组
 
     # 增大数组物理尺寸
-    if logicalSize == len(my_array):
-        temp = Array(len(my_array) * 2)  # 创建一个新数组
-        for i in range(logicalSize):
-            temp[i] = my_array[i]  # 从原数组复制内容到新数组
-        my_array = temp  # 把新数组赋值给原数组
+    while logicalSize < DEFAULT_CAPACITY * 2:
+        logicalSize += 1
+        if logicalSize == len(my_array):  # 触发条件
+            temp = Array(len(my_array) + 1)  # 创建一个新数组
+            for i in range(logicalSize):
+                temp[i] = my_array[i]  # 从原数组复制内容到新数组
+            my_array = temp  # 把新数组赋值给原数组
 
+    print("The array after increased is: ", my_array.items)  # 打印输出数组
+
+    # 减小数组物理尺寸
+    while logicalSize > len(my_array) // 4:
+        logicalSize -= 1
+        if logicalSize <= len(my_array) // 4 and len(my_array) >= DEFAULT_CAPACITY * 2:  # 触发条件
+            temp = Array(len(my_array) // 2)  # 创建一个新数组
+            for i in range(logicalSize):
+                temp[i] = my_array[i]  # 从原数组复制内容到新数组
+            my_array = temp  # 把新数组赋值给原数组
+    
+    print("The array after decreased is: ", my_array.items)  # 打印输出数组
 
 if __name__ == "__main__":
     main()
@@ -60,3 +77,5 @@ if __name__ == "__main__":
 # Initial array is:  [None, None, None, None, None]
 # Len of the array:  5
 # The array is:  [0, 1, 2, 3, 4]
+# The array after increased is:  [0, 1, 2, 3, 4, None, None, None, None, None, None]
+# The array after decreased is:  [0, 1, None, None, None]
